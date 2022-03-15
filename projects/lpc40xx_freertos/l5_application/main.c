c89605a0592177d9dcfc2d2daed2e150aaa3515c
tann1 <gadkaritanishq@gmail.com>
2022-03-14 20:25:31 -0700

    finished part 2

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -13,16 +13,17 @@ void uart_read_task(void *p) {
   char get_char;
   while (1) {
     // TODO: Use uart_lab__polled_get() function and printf the received value
-    uart_lab__polled_get(UART_3, &get_char);
+    uart_lab__get_char_from_queue(&get_char, 500);
+    // uart_lab__polled_get(UART_3, &get_char);
     fprintf(stderr, "%c\n", get_char);
-    vTaskDelay(500);
+    // vTaskDelay(500);
   }
 }
 
 void uart_write_task(void *p) {
   while (1) {
     // TODO: Use uart_lab__polled_put() function and send a value
-    uart_lab__polled_put(UART_3, 'B');
+    uart_lab__polled_put(UART_3, 'c');
     vTaskDelay(500);
   }
 }
@@ -31,7 +32,8 @@ int main(void) {
 
   puts("Starting RTOS");
 
-  uart_lab__init(UART_3, clock__get_peripheral_clock_hz(), 1);
+  uart_lab__init(UART_3, clock__get_peripheral_clock_hz(), 38400);
+  uart__enable_receive_interrupt(UART_3);
   gpio__construct_with_function(GPIO__PORT_0, 0, GPIO__FUNCTION_2); // u3 txd
   gpio__construct_with_function(GPIO__PORT_0, 1, GPIO__FUNCTION_2); // u3 rxd
 
--- projects/lpc40xx_freertos/l5_application/uart_lab/uart_lab.c
+++ projects/lpc40xx_freertos/l5_application/uart_lab/uart_lab.c
@@ -1,6 +1,8 @@
 #include "uart_lab.h"
 
-LPC_UART_TypeDef *temp_uart;
+static QueueHandle_t your_uart_rx_queue;
+
+LPC_UART_TypeDef *temp_uart, *interrupt_uart;
 
 void uart_lab__init(uart_number_e uart, uint32_t peripheral_clock, uint32_t baud_rate) {
   // Refer to LPC User manual and setup the register bits correctly
@@ -42,4 +44,44 @@ bool uart_lab__polled_put(uart_number_e uart, char output_byte) {
   // b) Copy output_byte to THR register
   temp_uart->THR = output_byte;
   return true;
+}
+
+static void your_receive_interrupt(void) {
+  // TODO: Read the IIR register to figure out why you got interrupted
+  if ((interrupt_uart->IIR & 0b1)) // meaning no interrupt pending Table.398
+    return;
+
+  // TODO: Based on IIR status, read the LSR register to confirm if there is data to be read
+  if (!(((interrupt_uart->IIR >> 1) & 0b111) == 0x2)) // if it's not a recieve interrupt
+    return;
+  // TODO: Based on LSR status, read the RBR register and input the data to the RX Queue
+  if (!(interrupt_uart->LSR & 0b1)) // if data isn't actually ready
+    return;
+  const char byte = interrupt_uart->RBR;
+  xQueueSendFromISR(your_uart_rx_queue, &byte, NULL);
+}
+
+// Public function to enable UART interrupt
+// TODO Declare this at the header file
+void uart__enable_receive_interrupt(uart_number_e uart_number) {
+  // TODO: Use lpc_peripherals.h to attach your interrupt
+  IRQn_Type IRQ_en = uart_number == UART_2 ? UART2_IRQn : UART3_IRQn;
+  lpc_peripheral_e peripheral_en = uart_number == UART_2 ? LPC_PERIPHERAL__UART2 : LPC_PERIPHERAL__UART3;
+
+  NVIC_EnableIRQ(IRQ_en);
+  lpc_peripheral__enable_interrupt(peripheral_en, your_receive_interrupt, "uart");
+
+  // TODO: Enable UART receive interrupt by reading the LPC User manual
+  // Hint: Read about the IER register
+  temp_uart = uart_number == UART_2 ? LPC_UART2 : LPC_UART3;
+  interrupt_uart = temp_uart;
+  temp_uart->IER = 0b1; //  enable recieve interrupt
+  // TODO: Create your RX queue
+  your_uart_rx_queue = xQueueCreate(10, sizeof(char));
+}
+
+// Public function to get a char from the queue (this function should work without modification)
+// TODO: Declare this at the header file
+bool uart_lab__get_char_from_queue(char *input_byte, uint32_t timeout) {
+  return xQueueReceive(your_uart_rx_queue, input_byte, timeout);
 }
