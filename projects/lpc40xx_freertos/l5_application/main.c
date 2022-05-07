20d7dce6b312c5aa9f0ed1464c1606a12ef626de
usernameTann <tanishq.gadkari@sjsu.edu>
2022-05-07 01:28:21 -0700

    added modularity

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -10,23 +10,14 @@
 #include "task.h"
 
 static void task_one(void *task_parameter) {
-  size_t idx = 0;
-  size_t song_size = sl__get_count();
-  char buffer_msg[MAX_SONG_FILE_SIZE + 4]; // <song num, 2><., 1>< , 1><song name, 32>
-  sl__refresh_list();
-
+  mp3__init(); // mp3.h
   while (1) {
-    sprintf(buffer_msg, "%u. %s\n", (idx + 1) % song_size, sl__get_item(idx++).filename);
-    lcd__send_row(buffer_msg);
     vTaskDelay(100);
   }
 }
 
 int main(void) {
-  lcd__init(9600);
-
-  xTaskCreate(task_one, "lcd transmitter", 1024, NULL, PRIORITY_LOW, NULL);
-
+  xTaskCreate(task_one, "mp3 init", 1024, NULL, PRIORITY_LOW, NULL);
   vTaskStartScheduler();
   return 0;
 }
--- projects/lpc40xx_freertos/l5_application/mp3/mp3.h
+++ projects/lpc40xx_freertos/l5_application/mp3/mp3.h
@@ -1,2 +1,6 @@
+#pragma once
+
 #include "lcd_driver.h"
 #include "song_list.h"
+
+void mp3__init(void);
